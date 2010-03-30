8a296bbbab3f0cba272932d33068d8a020655e22
minmey <minmey@54ab57f6-ac2e-531a-a680-8fe067e1323f>
2010-03-30 22:26:47 +0000

    Se agrego la clase Test para realizar pruebas

---
+++ trunk/Casa/src/principal/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package principal;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class Test {
+
+    public Test()
+    {
+
+    }
+
+    public void test()
+    {
+        
+    }
+
+}

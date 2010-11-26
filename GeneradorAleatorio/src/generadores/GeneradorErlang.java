2d145d0ff4387def037613e46c11b41cf9bcdf5d
minmey <minmey@54ab57f6-ac2e-531a-a680-8fe067e1323f>
2010-11-26 13:58:41 +0000

    solucionado el tema del generador erlang y weibull

--- GeneradorAleatorio/src/generadores/GeneradorErlang.java
+++ GeneradorAleatorio/src/generadores/GeneradorErlang.java
@@ -5,7 +5,6 @@
 
 package generadores;
 
-import cern.jet.random.Distributions;
 import cern.jet.random.engine.RandomEngine;
 import distribuciones.Erlang;
 
@@ -20,11 +19,13 @@ public class GeneradorErlang implements Generador
     private GeneradorCongruencial congruencial;
     private boolean banderaGenerador = true; //false = generador congruencial
     private int semilla = -1;
+    private RandomEngine randomGenerator;
 
     public GeneradorErlang(Erlang erlang)
     {
         this.erlang = erlang;
         this.banderaGenerador = true;
+        this.randomGenerator = RandomEngine.makeDefault();
     }
 
     public GeneradorErlang(Erlang erlang, int semilla)
@@ -32,6 +33,7 @@ public class GeneradorErlang implements Generador
         this.semilla = semilla;
         this.erlang = erlang;
         this.banderaGenerador = true;
+        this.randomGenerator = new cern.jet.random.engine.MersenneTwister(this.semilla);
     }
 
     public GeneradorErlang(Erlang erlang, String congruencial)
@@ -46,13 +48,25 @@ public class GeneradorErlang implements Generador
     {
         double numero = 0.0;
         if(this.banderaGenerador)
-            if(this.semilla == -1)
-                numero = Distributions.nextErlang(this.erlang.getVarianza(), this.erlang.getMedia(), RandomEngine.makeDefault());
-            else
-                numero = Distributions.nextErlang(this.erlang.getVarianza(), this.erlang.getMedia(), new cern.jet.random.engine.MersenneTwister(this.semilla));
+//            if(this.semilla == -1)
+//                numero = Distributions.nextErlang(this.erlang.getVarianza(), this.erlang.getMedia(), RandomEngine.makeDefault());
+//            else
+//                numero = Distributions.nextErlang(this.erlang.getVarianza(), this.erlang.getMedia(), new cern.jet.random.engine.MersenneTwister(this.semilla));
+            numero = this.nextErlang(this.erlang.getVarianza(), this.erlang.getMedia());
         else
             numero = this.congruencial.nextDouble();
         return numero;
+    }
+
+    private double nextErlang(double variance, double mean)
+    {
+	int k = (int)( (mean * mean ) / variance + 0.5 );
+	k = (k > 0) ? k : 1;
+	double a = k / mean;
+
+	double prod = 1.0;
+	for (int i = 0; i < k; i++) prod *= randomGenerator.raw();
+	return -Math.log(prod)/a;
     }
 
 }

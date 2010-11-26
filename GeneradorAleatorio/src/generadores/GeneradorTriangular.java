1b06826e2a501dd5ba060a0f2e7cfbbb9ad2da1f
minmey <minmey@54ab57f6-ac2e-531a-a680-8fe067e1323f>
2010-11-26 04:55:24 +0000

    solucionado el tema del generador triangular

--- GeneradorAleatorio/src/generadores/GeneradorTriangular.java
+++ GeneradorAleatorio/src/generadores/GeneradorTriangular.java
@@ -21,11 +21,13 @@ public class GeneradorTriangular implements Generador
     private GeneradorCongruencial congruencial;
     private boolean banderaGenerador = true; //false = generador congruencial
     private int semilla = -1;
+    private RandomEngine randomGenerator;
 
     public GeneradorTriangular(Triangular triangular)
     {
         this.banderaGenerador = true;
         this.triangular = triangular;
+        this.randomGenerator = RandomEngine.makeDefault();
     }
 
     public GeneradorTriangular(Triangular triangular, int semilla)
@@ -33,6 +35,7 @@ public class GeneradorTriangular implements Generador
         this.semilla = semilla;
         this.banderaGenerador = true;
         this.triangular = triangular;
+        this.randomGenerator = new cern.jet.random.engine.MersenneTwister(this.semilla);
     }
 
 
@@ -46,14 +49,30 @@ public class GeneradorTriangular implements Generador
     public double getNumero()
     {
         double numero = 0.0;
+//        if(this.banderaGenerador)
+//            if(this.semilla == -1)
+//                numero = Distributions.nextTriangular(triangular.getMinimo(), triangular.getMaximo(), triangular.getMedio(), RandomEngine.makeDefault());
+//            else
+//                numero = Distributions.nextTriangular(triangular.getMinimo(), triangular.getMaximo(), triangular.getMedio(), new cern.jet.random.engine.MersenneTwister(this.semilla));
+//        else
+//            numero = this.congruencial.nextDouble();
         if(this.banderaGenerador)
-            if(this.semilla == -1)
-                numero = Distributions.nextTriangular(triangular.getMinimo(), triangular.getMaximo(), triangular.getMedio(), RandomEngine.makeDefault());
-            else
-                numero = Distributions.nextTriangular(triangular.getMinimo(), triangular.getMaximo(), triangular.getMedio(), new cern.jet.random.engine.MersenneTwister(this.semilla));
+            numero = this.generar(this.triangular.getMinimo(), this.triangular.getMaximo(), this.triangular.getMedio());
         else
             numero = this.congruencial.nextDouble();
         return numero;
+    }
+
+    private double generar(double min, double max, double medio)
+    {
+
+        double t = (medio-min)/(max-min);
+        double u;
+	u=randomGenerator.raw();
+	if (u<t)
+            return(min+Math.sqrt((medio-min)*(max-min)*u));                      /* -1 <= x <= 0 */
+	else
+            return(max-Math.sqrt((max-medio)*(max-min)*(1-u)));                 /*  0 <= x <= 1 */
     }
 
 }

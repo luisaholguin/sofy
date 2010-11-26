da6d6e20bc7c7a8b620f011d0ba96438f5da214e
minmey <minmey@54ab57f6-ac2e-531a-a680-8fe067e1323f>
2010-11-26 13:58:41 +0000

    solucionado el tema del generador erlang y weibull

--- trunk/GeneradorAleatorio/src/generadores/GeneradorWeibull.java
+++ trunk/GeneradorAleatorio/src/generadores/GeneradorWeibull.java
@@ -20,11 +20,13 @@ public class GeneradorWeibull implements Generador
     private GeneradorCongruencial congruencial;
     private boolean banderaGenerador = true; //false = generador congruencial
     private int semilla = -1;
+    private RandomEngine randomGenerator;
 
     public GeneradorWeibull(Weibull weibull)
     {
         this.weibull = weibull;
         this.banderaGenerador = true;
+        this.randomGenerator = RandomEngine.makeDefault();
     }
 
     public GeneradorWeibull(Weibull weibull, int semilla)
@@ -32,6 +34,7 @@ public class GeneradorWeibull implements Generador
         this.semilla = semilla;
         this.weibull = weibull;
         this.banderaGenerador = true;
+        this.randomGenerator = new cern.jet.random.engine.MersenneTwister(this.semilla);
     }
 
     public GeneradorWeibull(Weibull weibull, String congruencial)
@@ -45,14 +48,21 @@ public class GeneradorWeibull implements Generador
         double numero = 0.0;
         if(this.banderaGenerador)
             if(this.semilla == -1)
-                numero = Distributions.nextWeibull(this.weibull.getAlfa(), this.weibull.getBeta(), RandomEngine.makeDefault());
-            else
-                numero = Distributions.nextWeibull(this.weibull.getAlfa(), this.weibull.getBeta(), new cern.jet.random.engine.MersenneTwister(this.semilla));
+//                numero = Distributions.nextWeibull(this.weibull.getAlfa(), this.weibull.getBeta(), RandomEngine.makeDefault());
+//            else
+//                numero = Distributions.nextWeibull(this.weibull.getAlfa(), this.weibull.getBeta(), new cern.jet.random.engine.MersenneTwister(this.semilla));
+                numero = this.nextWeibull(this.weibull.getAlfa(), this.weibull.getBeta());
         else
             numero = this.congruencial.nextDouble();
         return numero;
     }
 
 
+    private double nextWeibull(double alpha, double beta)
+    {
+	// Polar method.
+	// See Simulation, Modelling & Analysis by Law & Kelton, pp259
+	return Math.pow(beta * ( - Math.log(1.0 - randomGenerator.raw()) ), 1.0 / alpha);
+    }
 
 }

a428c1f8d18dbc696e900758753fbceb710b665e
minmey <minmey@54ab57f6-ac2e-531a-a680-8fe067e1323f>
2010-12-06 15:43:10 +0000

    agregado el generador congruencial mixto y soluiconado el problema del test de ciclos

--- trunk/GeneradorAleatorio/src/generadores/GeneradorCongruencial.java
+++ trunk/GeneradorAleatorio/src/generadores/GeneradorCongruencial.java
@@ -11,15 +11,62 @@ package generadores;
  */
 public class GeneradorCongruencial
 {
+    private double semilla = 1;
+    private double a = 6;
+    private double m = 13;
+    private double b = 0;
 
     public GeneradorCongruencial()
     {
     }
 
+    public GeneradorCongruencial(double semilla, double a, double m, double b) {
+        this.semilla = semilla;
+        this.a = a;
+        this.m = m;
+        this.b = b;
+    }
+
+
+
     public double nextDouble()
     {
-        double numero = 0.0;
-        return numero;
+        this.semilla = ((semilla*a)+b)%m;
+        return this.semilla;
     }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getM() {
+        return m;
+    }
+
+    public void setM(double m) {
+        this.m = m;
+    }
+
+    public double getSemilla() {
+        return semilla;
+    }
+
+    public void setSemilla(double semilla) {
+        this.semilla = semilla;
+    }
+
+    
 
 }
